89e17e4a1415782854bc7c1f0171f02787f6646a
ryamada1015 <rikakoyamada.0927@gmail.com>
2023-10-19 09:30:16 -0700

    added user authentication feature

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
+const validator = require("validator")
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
@@ -17,6 +18,19 @@ const userSchema = new Schema({
 // mongoose lets you to create a static method which can be called within the model allowing encapsulation
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled")
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid")
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough")
+  }
+
   const exists = await this.findOne({ email })
   if (exists) {
     throw Error("Email already in use")
@@ -27,6 +41,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt)
 
   const user = await this.create({ email, password: hash })
+
+  return user
+}
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled")
+  }
+
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error("Email does not exist")
+  }
+
+  // verify passowrd
+  const match = await bcrypt.compare(password, user.password)
+
+  if (!match) {
+    throw Error("Incorrect password")
+  }
 
   return user
 }
