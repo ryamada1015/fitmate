5274ef47562dcb1a0959b17a51af99111055f254
ryamada1015 <rikakoyamada.0927@gmail.com>
2023-10-24 15:37:46 -0700

    add frontend as regular content

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext"
+import logo from "../images/logo.png"
+
+const Navbar = () => {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <img src={logo} className="logo" alt="Fitmate" />
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
